8382527dcc03e1d8270aac60eabfb8a3f85cc165
TadaoYamaoka <tadaoyamaoka@gmail.com>
2017-05-24 20:21:40 +0900

    ElmoTeacherDecoder

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import shogi
 from ElmoTeacherDecoder import *
+import os
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('file')
-parser.add_argument('num', type=int)
+parser.add_argument('--num', type=int)
 args = parser.parse_args()
 
 with open(args.file, 'rb') as f:
-    for i in range(args.num):
+    filesize = os.fstat(f.fileno()).st_size
+    if args.num:
+        num = args.num
+    else:
+        num = int(filesize / sizeof(HuffmanCodedPosAndEval))
+    for i in range(num):
         hcpe = HuffmanCodedPosAndEval()
         f.readinto(hcpe)
         board, eval, bestMove, gameResult = decode_hcpe(hcpe)
