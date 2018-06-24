f22e7be205df3418e2ab9fb78a38d0ba3890a9ea
TadaoYamaoka <tadaoyamaoka@gmail.com>
2018-06-24 13:50:35 +0900

    7手詰めのテスト局面

--- extract_mated_hcp/extract_mated_hcp.cpp
+++ extract_mated_hcp/extract_mated_hcp.cpp
@@ -26,7 +26,7 @@ void test1()
 	pos.undoMove(move);*/
 
 	vector<string> sfens = {
-		/*"1n1g3+Pl/k1p1s4/1ng5p/pSP1p1pp1/1n3p3/P1K3P1P/1P7/9/L1G5L b 2R2BG2SL5Pn 161", // mate 15
+		"1n1g3+Pl/k1p1s4/1ng5p/pSP1p1pp1/1n3p3/P1K3P1P/1P7/9/L1G5L b 2R2BG2SL5Pn 161", // mate 15
 		"ln6K/9/1sp2+P3/pp4G1p/6P2/+rl+B+R5/k8/+b8/9 b 2G2SNL2Pgs2nl10p 1", // mate 15
 		"ln1s+R3K/2s6/p1pp1p3/kp+r4pp/N3p4/1Sg6/P2B2P1P/5g3/LL3g1NL b BGS2Pn5p 1", // mate 17
 		"n1gg2+R2/l2l5/p1k1p3K/1ppp5/3PBp3/9/P4P2P/g8/8L b RG2S2NL6Pb2sn2p 1", // mate 33
@@ -54,11 +54,8 @@ void test1()
 		"knS5K/llGp3G1/pp2+R2S1/2n6/9/6S2/P3+n3P/2P2P2L/6GNb b RBGSL11P 1", // mate 5
 		"ln5+LK/1rk1+B2S1/p1sp5/4p1pp1/1PPP1P3/2S1P3+l/P1B2S3/1R2G2+p1/LN3G3 b 2GN5Pnp", // mate 13
 		"1p+Bnn+R2K/3+R5/4ps1pp/3p2p2/1NG1s4/6kPP/P2PP4/3G1+s1G1/L8 b BSN3L6Pgp 1", // mate 11
-		"l2g2p1+P/1k2n4/ppns5/2pb2g1+L/4PP1pK/PP5S1/3+b1+s2P/7P1/8+r w 4Pr2gs2n2l2p 1", // mate 11*/
-		"ln2k2+L1/2gs1g3/2pppPs1p/1P7/5+B1P1/p1PPS4/P3P1P1P/1+r2g1GR1/1+b4KNL w 2NL2Ps2p 1", // mate 7
-		"6sn1/5gk1l/1P1s1pppb/2pn4p/2gS+B1PP1/1p7/1+RP2P2P/1K1+p5/1N1r4L w 2GSN2L5P 16", // mate 7
-		"l5s1l/1g4gk1/p3ppppp/9/7N1/1+RPp4P/P3+bPP1K/9/3G1SsNL w RNL4Pbgsn2p 18", // mate 7
-		/*"lns3kn1/1r7/4pgs+R1/2pp1p3/pp2P4/2P1SP3/PPSP5/2GBG4/LN1K3N+l b BG3Pl3p 49", // •s‹l‚Ý
+		"l2g2p1+P/1k2n4/ppns5/2pb2g1+L/4PP1pK/PP5S1/3+b1+s2P/7P1/8+r w 4Pr2gs2n2l2p 1", // mate 11
+		"lns3kn1/1r7/4pgs+R1/2pp1p3/pp2P4/2P1SP3/PPSP5/2GBG4/LN1K3N+l b BG3Pl3p 49", // •s‹l‚Ý
 		"lns4n1/1r3k3/4pgsg1/2pp1p3/pp2P4/2P1SP3/PPSP5/2GBG2R1/LN1K3N+l b B3Pl3p 47", // •s‹l‚Ý
 		"7nl/5Psk1/1+P1+P1p1pp/K3g4/6p1B/1SP4P1/PsS3P1P/1N7/+r6NL w GLrb2gnl6p 1", // •s‹l‚Ý
 		"ln3+P1+PK/1rk4+B1/3p1+L1+S1/p1p2p1+B1/3+r3s1/7s1/4p1+n+pp/+p3+n2p+p/1+p3+p+p+p+p b 2GN2L2gsp 1", // •s‹l‚Ý
@@ -67,7 +64,7 @@ void test1()
 		"l6GK/2p2+R1P1/p1nsp2+Sp/1p1p2s2/2+R2bk2/3P4P/P4+p1g1/2s6/L7L b B2GNL2n7p 1", // •s‹l‚Ý
 		"1n3G1nK/2+r2P3/p3+P1n1p/2p2Gp2/5l3/3P5/P1P3S2/6+Bpg/L1S1L3k b R2SNL5Pbg3p 1", // •s‹l‚Ý
 		"+B2B1n2K/7+R1/p2p1p1ps/3g2+r1k/1p3n3/4n1P+s1/PP7/1S7/L8 b 3GSL7Pn2l3p 1", // •s‹l‚Ý
-		"ln2g3l/2+Rskg3/p2sppL2/2pp1sP1p/2P2n3/B2P1N1p1/P1NKPP2P/1G1S1+p1P1/7+rL b B2Pg 98", // •s‹l‚Ý*/
+		"ln2g3l/2+Rskg3/p2sppL2/2pp1sP1p/2P2n3/B2P1N1p1/P1NKPP2P/1G1S1+p1P1/7+rL b B2Pg 98", // •s‹l‚Ý
 	};
 
 	for (string sfen : sfens) {
@@ -137,6 +134,77 @@ void test3()
 	pos.undoMove(move);
 }
 
+void test4()
+{
+	// 7Žè‹l‚ß
+	Position pos;
+
+	vector<string> sfens = {
+		"1pp4nl/2s2ggk1/2sppN1p1/5+Rp2/6P1p/1S+BG1PNK1/P2PPbR1P/2+p3L2/5+p1NL w 2Pgslp 1",
+		"l6nl/4g2g1/4k1bp1/+R1Spp3p/2P2pp2/PnnG1P2P/1P1PPS1R1/1SpG5/L1sK4L w 4Pbn 1",
+		"5+R1nl/9/1p1p2sp1/4pLk1p/p3+b1p2/1+RB2P+s1P/P3+p3K/6GL1/3P2GNL b 2GS3Ps2n3p 1",
+		"ln2+R4/2s6/k1pps4/3g1p+r1p/pP3n3/1pP5P/P1SGp+b3/2KG1B3/LN4PNL b GSL2P4p 1",
+		"5+R1nl/9/1p1p3p1/4ps2p/p3+b1pk1/1+RB2P+ssP/P3+p4/6GLK/3P2GNL b 2GS3P2nl3p 1",
+		"ln2+R4/2s6/2pps4/kP1g1p+r1p/p4n3/1SP5P/P2Gp4/2KG1+b3/LN4PNL b GSL4Pb3p 1",
+		"l6nl/4g2g1/4k1bp1/p1Spp2Rp/1+RP2pp2/PnnG1P2P/1P1PPS3/1SpG5/L1sK4L w 3Pbn 1",
+		"1pp4nl/2s2ggk1/2sppN1p1/5+Rp2/5PP1p/1S+BG2NK1/P2PPb2P/2+p3L2/5+p1NL w R2Pgslp 1",
+		"l6nl/4g2g1/4k1bp1/p1Spp3p/1+RP2pp1P/PnnG1P3/1P1PPS1R1/1SpG5/L1sK4L w 3Pbn 1",
+		"k2g1g1nl/ls5ps/ppnB+Nppbp/2p1p4/9/P1PP1S3/1P1S1PN1P/3G3R1/L2K1GP2 b RPl2p 1",
+		"ln6l/3rg1sk1/2s3n1p/P2p+b1pp1/1Pp1pp1P1/1S3G3/LpP2+B2P/9/KNG3R1L w G3Psn2p 1",
+		"ln2+R4/2s6/2pps4/k2g1p+r1p/pP3n3/1pP5P/P1SGp+b3/2KG1B3/LN4PNL b GSL2P4p 1",
+		"ln2+R4/2s6/2pps4/3g1p+r1p/p4n3/1kP5P/P2Gp+b3/2KG1B3/LN4PNL b GSL3Ps5p 1",
+		"ln2+R4/2s6/2pps4/1P1g1p+r1p/pk3n3/2P5P/P2Gp+b3/2KG1B3/LN4PNL b GSL5Ps2p 1",
+		"5g1nl/5gsk1/+L+L1p1p1pp/4p1p2/1pB6/N1pPPP1P1/1P1S2P1P/+pr2GS1RL/2K4N1 w BSgn2p 1",
+		"ln2+R4/2s6/2pps4/kP1g1p+r1p/p4n3/1SP5P/P2G1+b3/2KG+pB3/LN4PNL b GSL4P3p 1",
+		"l1R1S3l/3gk1gs1/p2pppn1p/9/3n5/5BPp1/PP+pPPPN1P/4KSGP1/1+bS4RL b 2Pgnlp 1",
+		"k2g1g1nl/ls5ps/ppnB+Nppbp/2p1pl3/9/P1PP1S2P/1P1S1PN2/3G3R1/L2K1GP2 b RP2p 1",
+		"ln2k2nl/1r1sg4/1pppP4/3PS2pp/p3g4/2P3p1P/PP3P1PL/9/LN1GKG1N1 b 2BSrs3p 1",
+		"ln2+R4/2s6/k1pps4/1P1g1p+r1p/5n3/pSP5P/P2Gp+b3/2KG1B3/LN4PNL b GSL4P3p 1",
+		"lnsgr1s+Pl/3k1g3/1ppp1p1+Pp/4p+bp2/p8/2P1P4/PP1P1PP1P/3KGS1R1/+b1SG3NL w Nnl 1",
+		"+S4g1nl/9/5pkpp/p1pg5/1p2pS1P1/P1P2P1s1/1P2G1P1P/1K1G5/LN2r1bNL w SL3Prbnp 1",
+		"lnsgr1+P1l/3k1g3/1ppp1p1+Pp/4p+bp2/p8/2P1P4/PP1PKPP1P/4GS1R1/+b1SG3NL w SNnl 1",
+		"2kg3+P1/p1s6/2nrg3p/Ppppp1p2/5N3/1BPPPPPb1/1PN1SS2P/2G4+pL/+l4K3 w S2LPrgn 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP2PP/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+bP1/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/2P2+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"+S4g1nl/4P4/5pkpp/p1pg5/1p2pS1P1/P1P2P1s1/1P2+b1P1P/3G5/LNKPr2NL w SLPrbgnp 1",
+		"+S4g1nl/9/4Ppkpp/p1pg5/1p2pS1P1/P1P2P1s1/1P2+b1P1P/3G5/LNKPr2NL w SLPrbgnp 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/2P5p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"5g1nl/5gsk1/+L+L1pSp1pp/4p1p2/1pB3n2/N2PPP1P1/1P1GK1P1P/+pr3R2L/7N1 w BGP2s2p 1",
+		"5g1nl/5gsk1/+L+L1pSp1pp/4p1p2/1pB6/N2PPP1P1/1P1GK1P1P/+pr3g1RL/7N1 w BP2sn2p 1",
+		"5g1nl/5gsk1/+L+L1pSp1pp/4p1p2/1pB3n2/N2PPP1P1/1P1G2P1P/+pr3RK1L/7N1 w BGP2s2p 1",
+		"5g1nl/5gsk1/+L+L1pSp1pp/4p1p2/1pB6/N2PPP1P1/1+r4P1P/+p2KGS1RL/7N1 w BPgsn3p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/2G5p/1S+B2PNK1/P2PP3P/2+p3+b2/5+p1NL w R2Pgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P1PPP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/2P2+p1NL w RPgs2l2p 1",
+		"1pp4nl/1Ps2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/1PsppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/1P3+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/1P6p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/PP1PP3P/2+p3+b2/5+p1NL w RPgs2l2p 1",
+		"+S4g1nl/9/5pkpp/p1pg5/1p2pS1P1/P1P2P1s1/1P2+r1P1P/3S5/LNKP3NL w BL2Prb2gnp 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/1P+p3+b2/5+p1NL w RPgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/1P3+p1NL w RPgs2l2p 1",
+		"1pp3Rnl/2s2ggk1/2sppN3/5+RPp1/8p/1S+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w 2Pgs2l2p 1",
+		"1pp4nl/2s2ggk1/2sppN3/5+RPp1/2S5p/2+BG1PNK1/P2PP3P/2+p3+b2/5+p1NL w R2Pgs2l2p 1",
+		"+S4g1nl/9/5pkpp/p1pgP4/1p2pS1P1/P1P2P1s1/1P2+b1P1P/3G5/LNKPr2NL w SLPrbgnp 1",
+		"+S4g1nl/9/5pkpp/p1pg5/1p2pS1P1/P1P1PP1s1/1P2+b1P1P/3G5/LNKPr2NL w SLPrbgnp 1",
+		"+S4g1nl/4P4/5pkpp/p1pg5/1p2pS1P1/P1P2P1s1/1P2+b1P1P/3G5/LNKLr2NL w S2Prbgnp 1",
+	};
+
+	for (string sfen : sfens) {
+		pos.set(sfen, nullptr);
+		auto start = std::chrono::system_clock::now();
+		bool ret = mateMoveInOddPly(pos, 7);
+		auto end = std::chrono::system_clock::now();
+
+		auto time_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+		cout << ret << "\t";
+		cout << time_ns / 1000000.0 << endl;
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	initTable();
@@ -144,7 +212,7 @@ int main(int argc, char *argv[])
 	HuffmanCodedPos::init();
 	dfpn_init();
 
-	test1(); return 0;
+	test4(); return 0;
 
 	if (argc < 3) {
 		std::cout << "extract_mated_hcp input_hcp not_mated_hcp mated_hcp" << std::endl;
